c872a6beadd8ebe27316d69701b5505374310a54
taejin1221 <taejin7824@gmail.com>
2024-10-06 22:51:55 +0900

    Style: 코드 스타일 변경
    - 변수명 통일: *_strip -> *
    - 불필요 주석 제거
    - 이외 스타일 변경

--- input_checker.py
+++ input_checker.py
@@ -7,36 +7,34 @@ with open("settings.json", "r") as f:
 os.chdir(settings["directory"])
 
 with open("inputs", "r") as f:
-    inputs = f.read()
+    inputs = f.read().split("!@#")
 
 with open("outputs", "r") as f:
-    outputs = f.read()
+    outputs = f.read().split("!@#")
 
-acc = 0
-for idx, (inp, out) in enumerate(zip(inputs.split("!@#"), outputs.split("!@#"))):
-    inp_strip = inp.strip()
-    out_strip = out.strip()
-    # 두 줄 이상의 문장을 출력하기 위해 repr 사용
+accept = 0
+for idx, (inp, out) in enumerate(zip(inputs, outputs)):
+    inp, out = inp.strip(), out.strip()
+
     res = subprocess.run("./a.out", input=inp, stdout=subprocess.PIPE, text=True)
-    res_strip = res.stdout.strip()
+    res = res.stdout.strip()
 
     print(f"===== 테스트 {idx} =====")
     print("입력")
-    print(inp_strip)
+    print(inp)
     print("기댓값")
-    print(out_strip)
+    print(out)
     print("출력")
-    print(res_strip)
+    print(res)
 
     print("결과: ", end='')
-    if res_strip == out_strip:
-        acc += 1
+    if res == out:
+        accept += 1
         print("맞았습니다!!")
     else:
         print("틀렸습니다!!")
 
-input_num = idx + 1
-
 print("===== 최종 결과 =====")
-print(f"정답: {acc}, 오답: {input_num - acc}")
-print(f"정답률: {acc / input_num * 100:.2f}%")
+input_size = len(inputs)
+print(f"정답: {accept}, 오답: {input_size - accept}")
+print(f"정답률: {accept / input_size * 100:.2f}%")
