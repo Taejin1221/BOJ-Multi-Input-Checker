bb571a6d136bcc7fe9551f811eba81880202aaec
taejin1221 <taejin7824@gmail.com>
2024-10-06 23:38:36 +0900

    Feat: 출력 포맷 수정
    - 결과만 출력, 자세히 출력, 틀린 답만 출력 모드 추가
    - 최종 결과에 틀린 테스트 케이스 출력
    
    Related to: #4

--- input_checker.py
+++ input_checker.py
@@ -1,10 +1,15 @@
 import json
 import os
 import subprocess
+import sys
 
 with open("settings.json", "r") as f:
     settings = json.load(f)
 os.chdir(settings["directory"])
+
+verbose = 0
+if len(sys.argv) > 2 and (sys.argv[1] == "-v" or sys.argv[1] == "--verbose"):
+    verbose = int(sys.argv[2])
 
 with open("inputs", "r") as f:
     inputs = f.read().split("!@#")
@@ -13,6 +18,7 @@ with open("outputs", "r") as f:
     outputs = f.read().split("!@#")
 
 accept = 0
+fail_case = []
 for idx, (inp, out) in enumerate(zip(inputs, outputs)):
     inp, out = inp.strip(), out.strip()
 
@@ -20,20 +26,44 @@ for idx, (inp, out) in enumerate(zip(inputs, outputs)):
     res = res.stdout.strip()
 
     print(f"===== 테스트 {idx} =====")
-    print("입력")
-    print(inp, end='\n\n')
-    print("기댓값")
-    print(out, end='\n\n')
-    print("출력")
-    print(res, end='\n\n')
+    if verbose == 1:
+        print("입력")
+        print(inp, end='\n\n')
+        print("기댓값")
+        print(out, end='\n\n')
+        print("출력")
+        print(res, end='\n\n')
 
     if res == out:
         accept += 1
         print("맞았습니다!!")
     else:
         print("***** 틀렸습니다!! *****")
+        fail_case.append(idx)
+        if verbose == 1 or verbose == 2:
+            print("결과: 기댓값 vs 출력")
+            out_split, res_split = out.split('\n'), res.split('\n')
+            for out_one, res_one in zip(out_split, res_split):
+                if out_one == res_one:
+                    print("정답:", end=" ")
+                else:
+                    print("*오답*:", end=" ")
+                print(f"{out_one} vs {res_one}")
+
+            if len(out_split) > len(res_split):
+                print("*오답*:", end=" ")
+                for out_one in out_split[len(res_split):]:
+                    print(f"{out_one} vs ''")
+            elif len(out_split) < len(res_split):
+                print("*오답*:", end=" ")
+                for res_one in res_split[len(out_split):]:
+                    print(f"'' vs {res_one}")
+    print("")
 
 print("===== 최종 결과 =====")
 input_size = len(inputs)
 print(f"정답: {accept}, 오답: {input_size - accept}")
 print(f"정답률: {accept / input_size * 100:.2f}%")
+if input_size != accept:
+    print("오답 케이스:", end=" ")
+    print(", ".join(list(map(str, fail_case))))
